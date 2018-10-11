cfb8c3a6f4d45c5d7f8197725fecc547725e5fe7
zhuyeaini9 <37789892+zhuyeaini9@users.noreply.github.com>
2018-10-11 16:35:41 +0800

    Initial commit

---
+++ YeTrade/Common.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YeTrade
+{
+    public class CConfig
+    {
+        public static string mServer;
+        public static string mAccount;
+        public static string mToken;
+    }
+    public class CCandleData
+    {
+        public OHLC mPrice;
+        public DateTime mCandleTime;
+        public CCandleData(OHLC ol,string dateStr)
+        {
+            mPrice = ol;
+            mCandleTime = CHelp.str2Date(dateStr);
+        }
+        public CCandleData(OHLC ol,DateTime dt)
+        {
+            mPrice = ol;
+            mCandleTime = dt;
+        }
+    }
+    public struct OHLC
+    {
+        public double o { get; set; }
+        public double h { get; set; }
+        public double l { get; set; }
+        public double c { get; set; }
+    }
+    public class Candle
+    {
+        public bool complete { get; set; }
+        public int volume { get; set; }
+        public string time { get; set; }
+        public OHLC ask { get; set; }
+        public OHLC bid { get; set; }
+        public override string ToString()
+        {
+            return time + "," + ask.o + "," + bid.o + "," + ask.h + "," + bid.h + ","
+                + ask.l + "," + bid.l + "," + ask.c + "," + bid.c + "," + volume;
+        }
+    }
+    public class Instrument
+    {
+        public string instrument { get; set; }
+        public string granularity { get; set; }
+        public List<Candle> candles { get; set; }
+        public Instrument()
+        {
+            candles = new List<Candle>();
+        }
+    }
+}
---
+++ YeTrade/Help.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace YeTrade
+{
+    public class CHelp
+    {
+        public static void loadConfig()
+        {
+            CConfig.mServer = ConfigurationManager.AppSettings["Server"];
+            CConfig.mAccount = ConfigurationManager.AppSettings["Account"];
+            CConfig.mToken = ConfigurationManager.AppSettings["Token"];
+        }
+        public static DateTime str2Date(string s)
+        {
+            return XmlConvert.ToDateTime(s, "yyyy-MM-ddTHH:mm:ssZ");
+        }
+        public static string date2Str(DateTime s)
+        {
+            return XmlConvert.ToString(s, "yyyy-MM-ddTHH:mm:ssZ");
+        }
+
+        public static Dictionary<DateTime,CCandleData> getCandleHistoryData(string symbol,DateTime start,DateTime end, string gran)
+        {
+            Dictionary<DateTime, CCandleData> re = new Dictionary<DateTime, CCandleData>();
+
+            try
+            {
+                string requestString = CConfig.mServer + "/v3/instruments/" + symbol + "/candles";
+                requestString = requestString + "?" + "price=A&from=" + date2Str(start) + "&to=" + date2Str(end) + "&granularity=" + gran;
+                HttpWebRequest request = WebRequest.CreateHttp(requestString);
+                request.Headers[HttpRequestHeader.Authorization] = "Bearer " + CConfig.mToken;
+                request.Method = "GET";
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                    {
+                        throw new WebException("Web request error.");
+                    }
+                    Stream s = response.GetResponseStream();
+                    StreamReader sr = new StreamReader(s);
+                    var jr = new JsonTextReader(sr);
+                    var serializer = new JsonSerializer();
+                    Instrument im = serializer.Deserialize<Instrument>(jr);
+
+                    foreach(var v in im.candles)
+                    {
+                        DateTime dt = str2Date(v.time);
+                        if(!re.ContainsKey(dt))
+                        {
+                            re[dt] = new CCandleData(v.ask, dt);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            return re;
+        }
+    }
+}
